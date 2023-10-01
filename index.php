863dc9f3bae29420dec50eabf892ddab44d0007f
Louis Sofia <louis@sofia.org>
2023-10-01 10:35:49 +0700

    push code begin

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nurx</title>
+    <link rel="stylesheet" href="assets/css/nurx-style.css">
+</head>
+<body>
+    <!-- wapper or main-->
+    <div id="main">
+        <div class="container">
+            <div class="section-header">
+                <div class="sh-notification_bar">
+                    <p class="nb-text">medication as low as $0 with insurance or $15 per month without <a href="#" class="get-stated">Get started</a></p>
+                </div>
+                <div class="sh-nav">
+                    <div class="nav-left">
+                        <a href="#" class="nav-menu-dropdown">OUR SERVICES</a>
+                    </div>
+                    <div class="nav-center">
+                        <a href="/" class="nav-logo">
+                            <img src="assets/images/nurx-logo.svg" alt="nurx logo" class="nurx-logo">
+                        </a>
+                    </div>
+                    <div class="nav-right">
+                        <div class="nav-menu">
+                            <ul class="menu-nurx">
+                                <li class="menu-item"><a href="#" class="link-menu-item">blog</a></li>
+                                <li class="menu-item"><a href="#" class="link-menu-item menu-has-child">about</a></li>
+                                <li class="menu-item"><a href="#" class="link-menu-item">help & faqs</a></li>
+                                <li class="menu-item"><a href="#" class="link-menu-item nurx-icon-login">log in</a></li>
+                                <li class="menu-item"><a href="#" class="link-menu-item nurx-btn-menu">Get started</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="section-hero">
+
+            </div>
+            
+        </div>
+    </div>
+    
+</body>
+</html>
